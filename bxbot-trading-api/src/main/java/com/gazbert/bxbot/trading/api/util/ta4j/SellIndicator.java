486bfce2372dc52651e3fdc6bff63845bad09f1a
Marc Dahlem <m_dahlem@cs.uni-kl.de>
2021-05-09 01:15:18 +0200

    SellIndicators are constructed from caller

--- bxbot-trading-api/src/main/java/com/gazbert/bxbot/trading/api/util/ta4j/SellIndicator.java
+++ bxbot-trading-api/src/main/java/com/gazbert/bxbot/trading/api/util/ta4j/SellIndicator.java
@@ -4,31 +4,27 @@ import com.google.common.primitives.Ints;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.Indicator;
 import org.ta4j.core.indicators.CachedIndicator;
-import org.ta4j.core.indicators.helpers.HighPriceIndicator;
-import org.ta4j.core.indicators.helpers.HighestValueIndicator;
-import org.ta4j.core.indicators.helpers.LowPriceIndicator;
-import org.ta4j.core.indicators.helpers.TransformIndicator;
+import org.ta4j.core.indicators.helpers.*;
 import org.ta4j.core.num.Num;
 
 import java.math.BigDecimal;
 import java.util.TreeSet;
+import java.util.function.BiFunction;
 
 import static org.ta4j.core.num.NaN.NaN;
 
 /* This indicator is only active in a sell phase and when the last trade is still open. */
 public class SellIndicator extends TradeBasedIndicator<Num> {
 
-    private final Indicator<Num> indicator;
-    private final boolean useBuyTime;
+    private final BiFunction<Integer, Integer, Indicator<Num>> buyIndicatorCreator;
 
-    public SellIndicator(Indicator<Num> indicator, boolean useBuyTime) {
-        this(indicator, null, useBuyTime);
+    public SellIndicator(BarSeries series, BiFunction<Integer, Integer, Indicator<Num>> buyIndicatorCreator) {
+        this(series, null, buyIndicatorCreator);
     }
 
-    public SellIndicator(Indicator<Num> indicator, TradeBasedIndicator<Num> tradeKnowingIndicator, boolean useBuyTime) {
-        super(indicator.getBarSeries(), tradeKnowingIndicator);
-        this.indicator = indicator;
-        this.useBuyTime = useBuyTime;
+    public SellIndicator(BarSeries series, TradeBasedIndicator<Num> tradeKnowingIndicator, BiFunction<Integer, Integer, Indicator<Num>> buyIndicatorCreator) {
+        super(series, tradeKnowingIndicator);
+        this.buyIndicatorCreator = buyIndicatorCreator;
     }
 
     @Override
@@ -38,11 +34,7 @@ public class SellIndicator extends TradeBasedIndicator<Num> {
 
     @Override
     protected Num calculateLastTradeWasBuy(int buyIndex, int index) {
-        if (useBuyTime) {
-            return indicator.getValue(buyIndex);
-        } else {
-            return new HighestValueIndicator(indicator, index-buyIndex+1).getValue(index);
-        }
+        return buyIndicatorCreator.apply(buyIndex, index).getValue(index);
     }
 
     @Override
@@ -54,13 +46,13 @@ public class SellIndicator extends TradeBasedIndicator<Num> {
         BigDecimal buyFeeFactor = BigDecimal.ONE.add(buyFee);
         BigDecimal sellFeeFactor = BigDecimal.ONE.subtract(sellFee);
         TransformIndicator breakEvenCalculator = TransformIndicator.divide(TransformIndicator.multiply(new HighPriceIndicator(series), buyFeeFactor), sellFeeFactor);
-        return new SellIndicator(breakEvenCalculator, true);
+        return new SellIndicator(series, (buyIndex, index) -> new ConstantIndicator<>(series, breakEvenCalculator.getValue(buyIndex)));
     }
 
     public static SellIndicator createSellLimitIndicator(BarSeries series, BigDecimal limitPercentageUnderCurrentBid, SellIndicator tradeKnowingIndicator) {
         LowPriceIndicator bidPriceIndicator = new LowPriceIndicator(series);
         BigDecimal limitScaleFactor = BigDecimal.ONE.subtract(limitPercentageUnderCurrentBid);
         TransformIndicator sellLimitCalculator = TransformIndicator.multiply(bidPriceIndicator, limitScaleFactor);
-        return new SellIndicator(sellLimitCalculator, tradeKnowingIndicator, false);
+        return new SellIndicator(series, tradeKnowingIndicator, (buyIndex, index) -> new HighestValueIndicator(sellLimitCalculator, index-buyIndex+1));
     }
 }
