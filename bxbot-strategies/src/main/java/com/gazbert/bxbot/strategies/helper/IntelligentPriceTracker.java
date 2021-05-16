58d7272176643dd871b24a2c93658ad1f2a202a7
Marc Dahlem <m_dahlem@cs.uni-kl.de>
2021-05-16 18:51:41 +0200

    Download OHLC instead of ticker

--- bxbot-strategies/src/main/java/com/gazbert/bxbot/strategies/helper/IntelligentPriceTracker.java
+++ bxbot-strategies/src/main/java/com/gazbert/bxbot/strategies/helper/IntelligentPriceTracker.java
@@ -30,40 +30,56 @@ public class IntelligentPriceTracker {
     private final TradingApi tradingApi;
     private final Market market;
     private final BarSeries series;
-    private final static Map<Integer, Map<String, BigDecimal>> balances = new LinkedHashMap<>() {
-        @Override
-        protected boolean removeEldestEntry(final Map.Entry eldest) {
-            return size() > 10;
-        }
-    };
+    private static final Map<Integer, Map<String, BigDecimal>> balances =
+            new LinkedHashMap<>() {
+                @Override
+                protected boolean removeEldestEntry(final Map.Entry eldest) {
+                    return size() > 10;
+                }
+            };
     private final boolean shouldShowLiveChart;
 
     private String liveGraphID;
-    private final Collection<Ta4j2Chart.ChartIndicatorConfig> registeredLiveChartIndicatorConfigs = new LinkedList<>();
+    private final Collection<Ta4j2Chart.ChartIndicatorConfig> registeredLiveChartIndicatorConfigs =
+            new LinkedList<>();
+    private Integer resumeID = null;
 
     public IntelligentPriceTracker(TradingApi tradingApi, Market market, StrategyConfig config) {
         this.tradingApi = tradingApi;
         this.market = market;
-        this.series = new BaseBarSeriesBuilder().withName(market.getName() + "_" + System.currentTimeMillis()).build();
+        this.series =
+                new BaseBarSeriesBuilder()
+                        .withName(market.getName() + "_" + System.currentTimeMillis())
+                        .build();
         this.shouldShowLiveChart = StrategyConfigParser.readBoolean(config, "show-live-chart", false);
     }
 
-
     public void updateMarketPrices() throws ExchangeNetworkException, TradingApiException {
-        Ticker currentTicker = tradingApi.getTicker(market.getId());
-        LOG.info(() -> market.getName() + " Updated latest market info: " + currentTicker);
-        Long timestampInTicker = currentTicker.getTimestamp();
-        ZonedDateTime tickerTimestamp;
-        if(timestampInTicker == null) {
-            tickerTimestamp = ZonedDateTime.now();
-        } else {
-            tickerTimestamp = ZonedDateTime.ofInstant(Instant.ofEpochMilli(timestampInTicker), ZoneId.systemDefault());
+        Ohlc ohlcData = tradingApi.getOhlc(market.getId(), OhlcInterval.OneMinute, resumeID);
+        LOG.info(() -> market.getName() + " Updated latest market info: " + ohlcData);
+        resumeID = ohlcData.getResumeID();
+        for (OhlcFrame frame : ohlcData.getFrames()) {
+            if (isCompleted(frame)) {
+                ZonedDateTime startTime = frame.getTime();
+                ZonedDateTime endTime = startTime.plusMinutes(1);
+
+                series.addBar(
+                        Duration.between(startTime, endTime),
+                        endTime,
+                        frame.getOpen(),
+                        frame.getHigh(),
+                        frame.getLow(),
+                        frame.getClose(),
+                        frame.getVolume());
+            }
         }
-        series.addBar(Duration.ZERO, tickerTimestamp, currentTicker.getLast(), currentTicker.getAsk(), currentTicker.getBid(), currentTicker.getLast(), BigDecimal.ZERO);
+
         this.updateLiveGraph();
     }
 
-
+    private boolean isCompleted(OhlcFrame frame) {
+        return frame.getTime().toEpochSecond() <= resumeID;
+    }
 
     public BigDecimal getAsk() {
         return (BigDecimal) series.getLastBar().getHighPrice().getDelegate();
@@ -89,35 +105,51 @@ public class IntelligentPriceTracker {
         return formatWithCounterCurrency(getLast());
     }
 
-    public BigDecimal getAvailableCounterCurrencyBalance() throws ExchangeNetworkException, TradingApiException {
+    public BigDecimal getAvailableCounterCurrencyBalance()
+            throws ExchangeNetworkException, TradingApiException {
         return getBalance(market.getCounterCurrency());
     }
 
-    public BigDecimal getAvailableBaseCurrencyBalance() throws ExchangeNetworkException, TradingApiException {
+    public BigDecimal getAvailableBaseCurrencyBalance()
+            throws ExchangeNetworkException, TradingApiException {
         return getBalance(market.getBaseCurrency());
     }
 
-    private BigDecimal getBalance(String currency) throws ExchangeNetworkException, TradingApiException {
-        if(!balances.containsKey(getCurrentTick())) {
+    private BigDecimal getBalance(String currency)
+            throws ExchangeNetworkException, TradingApiException {
+        if (!balances.containsKey(getCurrentTick())) {
             loadAvailableBalancesFromServer();
         }
         Map<String, BigDecimal> currentAccountBalances = balances.get(getCurrentTick());
         final BigDecimal currentBalance = currentAccountBalances.get(currency);
         if (currentBalance == null) {
-            final String errorMsg = "Failed to get current currency balance as '" + currency + "' key is not available in the balances map. Balances available: " + currentAccountBalances;
+            final String errorMsg =
+                    "Failed to get current currency balance as '"
+                            + currency
+                            + "' key is not available in the balances map. Balances available: "
+                            + currentAccountBalances;
             LOG.warn(() -> errorMsg);
             return BigDecimal.ZERO;
         }
-        LOG.info(() -> market.getName() + "Currency balance available on exchange is ["+formatWithCurrency(currentBalance, currency) + "]");
+        LOG.info(
+                () ->
+                        market.getName()
+                                + "Currency balance available on exchange is ["
+                                + formatWithCurrency(currentBalance, currency)
+                                + "]");
         return currentBalance;
     }
 
-
-    private void loadAvailableBalancesFromServer() throws ExchangeNetworkException, TradingApiException {
+    private void loadAvailableBalancesFromServer()
+            throws ExchangeNetworkException, TradingApiException {
         LOG.info(() -> market.getName() + " Fetching all available balances from the exchange.");
         BalanceInfo balanceInfo = tradingApi.getBalanceInfo();
         Map<String, BigDecimal> loadedBalances = balanceInfo.getBalancesAvailable();
-        LOG.info(() -> market.getName() + " Loaded the following account balances from the exchange: " + loadedBalances);
+        LOG.info(
+                () ->
+                        market.getName()
+                                + " Loaded the following account balances from the exchange: "
+                                + loadedBalances);
         balances.put(getCurrentTick(), loadedBalances);
     }
 
@@ -125,11 +157,13 @@ public class IntelligentPriceTracker {
         return series.getEndIndex();
     }
 
-    public String getFormattedBaseCurrencyBalance() throws ExchangeNetworkException, TradingApiException {
+    public String getFormattedBaseCurrencyBalance()
+            throws ExchangeNetworkException, TradingApiException {
         return formatWithBaseCurrency(getAvailableBaseCurrencyBalance());
     }
 
-    public String getFormattedCounterCurrencyBalance() throws ExchangeNetworkException, TradingApiException {
+    public String getFormattedCounterCurrencyBalance()
+            throws ExchangeNetworkException, TradingApiException {
         return formatWithCounterCurrency(getAvailableCounterCurrencyBalance());
     }
 
@@ -161,7 +195,9 @@ public class IntelligentPriceTracker {
     private void updateLiveGraph() {
         if (shouldShowLiveChart) {
             if (liveGraphID == null) {
-                liveGraphID = Ta4j2Chart.createLiveChart(getSeries(), getLivechartIndicatorConfigs(), MAX_AMOUNT_LIVECHART_BARS);
+                liveGraphID =
+                        Ta4j2Chart.createLiveChart(
+                                getSeries(), getLivechartIndicatorConfigs(), MAX_AMOUNT_LIVECHART_BARS);
             } else {
                 Ta4j2Chart.updateLiveChart(liveGraphID);
             }
