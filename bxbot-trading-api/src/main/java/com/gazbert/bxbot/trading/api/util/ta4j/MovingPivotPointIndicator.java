102874453b5fb8dd39d5ddd20c5ebc96ef560b94
Marc Dahlem <m_dahlem@cs.uni-kl.de>
2021-06-03 12:39:07 +0200

    Cache confirmations to speed up computation massively

--- bxbot-trading-api/src/main/java/com/gazbert/bxbot/trading/api/util/ta4j/MovingPivotPointIndicator.java
+++ bxbot-trading-api/src/main/java/com/gazbert/bxbot/trading/api/util/ta4j/MovingPivotPointIndicator.java
@@ -1,10 +1,13 @@
 package com.gazbert.bxbot.trading.api.util.ta4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.Indicator;
 import org.ta4j.core.indicators.AbstractIndicator;
 import org.ta4j.core.num.Num;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -15,8 +18,15 @@ import static org.ta4j.core.num.NaN.NaN;
 public abstract class MovingPivotPointIndicator extends AbstractIndicator<Num> {
 
     private MovingPivotPointIndicator oppositPivotIndicator;
+    protected static final Logger LOG = LogManager.getLogger();
 
     private final Map<Integer, TreeSet<Integer>> reversalPoints = new LinkedHashMap<>() {
+        @Override
+        protected boolean removeEldestEntry(final Map.Entry eldest) {
+            return size() > 10;
+        }
+    };
+    private Map<Integer, Map<Integer, Boolean>> confirmations = new LinkedHashMap<>() {
         @Override
         protected boolean removeEldestEntry(final Map.Entry eldest) {
             return size() > 10;
@@ -71,6 +81,14 @@ public abstract class MovingPivotPointIndicator extends AbstractIndicator<Num> {
     }
 
     private boolean isConfirmed(int index) {
+        if (!confirmations.containsKey(getBarSeries().getEndIndex())) {
+            confirmations.put(getBarSeries().getEndIndex(), new HashMap<>());
+        }
+        Map<Integer, Boolean> currentTickConfirmations = confirmations.get(getBarSeries().getEndIndex());
+        if (currentTickConfirmations.containsKey(index)) {
+            return currentTickConfirmations.get(index);
+        }
+
         Num valueToCheck = getPivotIndicator().getValue(index);
         int endIndex = getBarSeries().getEndIndex();
         Num lastConfirmation = getConfirmationIndicator().getValue(index);
@@ -79,15 +97,18 @@ public abstract class MovingPivotPointIndicator extends AbstractIndicator<Num> {
         for (int inFrameIndex = index + 1; inFrameIndex <= endIndex; inFrameIndex++) {
             Num otherValue = getPivotIndicator().getValue(inFrameIndex);
             if (contradictsPivot(valueToCheck, otherValue)) {
+                currentTickConfirmations.put(index, false);
                 return false;
             }
             if (oppositPivotIndicator.isConfirmed(inFrameIndex)) {
+                currentTickConfirmations.put(index, false);
                 return false;
             }
             Num confirmationValue = getConfirmationIndicator().getValue(inFrameIndex);
             if (contradictsPivot(confirmationValue, lastConfirmation)) {
                 confirmations++;
                 if (confirmations >= 2) {
+                    currentTickConfirmations.put(index, true);
                     return true;
                 }
                 lastConfirmation = confirmationValue;
